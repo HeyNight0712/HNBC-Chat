3a3297d3d9a61002b4e3d425f58fc901717edeb4
blacknight0712 <blacknight0981@gmail.com>
2024-04-28 09:24:10 +0800

    初始提交

---
+++ src/main/java/heyblock0712/hnbcchat/HNBC_Chat.java
@@ -0,0 +1,16 @@
+package heyblock0712.hnbcchat;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class HNBC_Chat extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
